3de908c612f43bd64a2fc1104ace42bccdcb67c3
Kamil Wysocki <kamil.wysocki93@gmail.com>
2016-10-26 23:59:47 +0200

    Code refactoring

---
+++ navAnimation/ViewControllerB.swift
@@ -0,0 +1,47 @@
+//
+// Created by Kamil Wysocki on 26/10/16.
+// Copyright (c) 2016 k8mil. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewControllerB : UIViewController {
+
+    override func loadView() {
+        super.loadView()
+        title = "B"
+        view.backgroundColor = .white
+        setColors()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        animate()
+    }
+
+    override func willMove(toParentViewController parent: UIViewController?) {
+        navigationController?.navigationBar.barTintColor = .red
+        super.willMove(toParentViewController: parent)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        navigationController?.navigationBar.barTintColor = .blue
+    }
+
+    private func animate() {
+        guard let coordinator = self.transitionCoordinator else {
+            return
+        }
+        coordinator.animate(alongsideTransition: {
+            [weak self] context in
+            self?.setColors()
+        }, completion: nil)
+    }
+
+    private func setColors(){
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.barTintColor = .blue
+    }
+
+}
