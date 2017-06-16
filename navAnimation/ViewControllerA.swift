0b579448db996f49ca16d01326cf84ae793dc6ad
Kamil Wysocki <kamil.wysocki93@gmail.com>
2017-06-16 20:17:45 +0200

    Remove transitionCoordinator

--- navAnimation/ViewControllerA.swift
+++ navAnimation/ViewControllerA.swift
@@ -1,37 +1,26 @@
 import UIKit
 
-class ViewControllerA: UIViewController {
+    class ViewControllerA: UIViewController {
 
         override func loadView() {
             super.loadView()
             title = "A"
             view.backgroundColor = .white
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "NEXT", style: .plain, target: self, action: #selector(self.showController))
-            setColors()
         }
 
         override func viewWillAppear(_ animated: Bool) {
+            setColors()
             super.viewWillAppear(animated)
-            animate()
         }
 
         func showController() {
             navigationController?.pushViewController(ViewControllerB(), animated: true)
         }
 
-        private func animate() {
-            guard let coordinator = self.transitionCoordinator else {
-                return
-            }
-
-            coordinator.animate(alongsideTransition: {
-                [weak self] context in
-                self?.setColors()
-            }, completion: nil)
-        }
-
         private func setColors() {
             navigationController?.navigationBar.tintColor = .black
             navigationController?.navigationBar.barTintColor = .red
+            navigationController?.navigationBar.isTranslucent = false
         }
-}
+    }
--- navAnimation/ViewControllerB.swift
+++ navAnimation/ViewControllerB.swift
@@ -1,48 +1,30 @@
-//
-// Created by Kamil Wysocki on 26/10/16.
-// Copyright (c) 2016 k8mil. All rights reserved.
-//
+    import Foundation
+    import UIKit
 
-import Foundation
-import UIKit
+    class ViewControllerB: UIViewController {
 
-class ViewControllerB: UIViewController {
-
-    override func loadView() {
-        super.loadView()
-        title = "B"
-        view.backgroundColor = .white
-        setColors()
-    }
-
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        animate()
-    }
-
-    override func willMove(toParentViewController parent: UIViewController?) {
-        // tricky part in iOS 10
-        navigationController?.navigationBar.barTintColor = .red //previous color
-        super.willMove(toParentViewController: parent)
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        navigationController?.navigationBar.barTintColor = .blue
-    }
-
-    private func animate() {
-        guard let coordinator = self.transitionCoordinator else {
-            return
+        override func loadView() {
+            super.loadView()
+            title = "B"
+            view.backgroundColor = .white
         }
-        coordinator.animate(alongsideTransition: {
-            [weak self] context in
-            self?.setColors()
-        }, completion: nil)
-    }
 
-    private func setColors() {
-        navigationController?.navigationBar.tintColor = .black
-        navigationController?.navigationBar.barTintColor = .blue
-    }
+        override func viewWillAppear(_ animated: Bool) {
+            setColors()
+            super.viewWillAppear(animated)
+        }
 
-}
+        override func willMove(toParentViewController parent: UIViewController?) {
+            if parent == nil {
+                navigationController?.navigationBar.barTintColor = .red
+            }
+            super.willMove(toParentViewController: parent)
+        }
+
+
+        private func setColors() {
+            navigationController?.navigationBar.tintColor = .black
+            navigationController?.navigationBar.barTintColor = .blue
+            navigationController?.navigationBar.isTranslucent = false
+        }
+    }
