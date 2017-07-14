9f0e5a24f387bad0e89284a83da3c989d1b9f7ff
Kamil Wysocki <kamil.wysocki@brightinventions.pl>
2017-07-14 10:57:46 +0200

    WIP on navBar height animation

--- navAnimation/ViewControllerA.swift
+++ navAnimation/ViewControllerA.swift
@@ -1,26 +1,34 @@
 import UIKit
 
-    class ViewControllerA: UIViewController {
+class ViewControllerA: UIViewController {
 
-        override func loadView() {
-            super.loadView()
-            title = "A"
-            view.backgroundColor = .white
-            navigationItem.rightBarButtonItem = UIBarButtonItem(title: "NEXT", style: .plain, target: self, action: #selector(self.showController))
-        }
-
-        override func viewWillAppear(_ animated: Bool) {
-            setColors()
-            super.viewWillAppear(animated)
-        }
-
-        func showController() {
-            navigationController?.pushViewController(ViewControllerB(), animated: true)
-        }
-
-        private func setColors() {
-            navigationController?.navigationBar.tintColor = .black
-            navigationController?.navigationBar.barTintColor = .red
-            navigationController?.navigationBar.isTranslucent = false
-        }
+    override func loadView() {
+        super.loadView()
+        title = "A"
+        view.backgroundColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "NEXT", style: .plain, target: self, action: #selector(self.showController))
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setColors()
+        applyNavigationBarHeight(44)
+        super.viewWillAppear(animated)
+    }
+
+    func showController() {
+        navigationController?.pushViewController(ViewControllerB(), animated: true)
+    }
+
+    private func setColors() {
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.barTintColor = .red
+        navigationController?.navigationBar.isTranslucent = false
+    }
+}
+
+//extension UINavigationBar {
+//    open override func sizeThatFits(_ size: CGSize) -> CGSize {
+//        let screenRect = UIScreen.main.bounds
+//        return CGSize(width: screenRect.size.width, height: 44)
+//    }
+//}
--- navAnimation/ViewControllerB.swift
+++ navAnimation/ViewControllerB.swift
@@ -1,30 +1,55 @@
-    import Foundation
-    import UIKit
+import Foundation
+import UIKit
 
-    class ViewControllerB: UIViewController {
+class ViewControllerB: UIViewController {
 
-        override func loadView() {
-            super.loadView()
-            title = "B"
-            view.backgroundColor = .white
-        }
-
-        override func viewWillAppear(_ animated: Bool) {
-            setColors()
-            super.viewWillAppear(animated)
-        }
-
-        override func willMove(toParentViewController parent: UIViewController?) {
-            if parent == nil {
-                navigationController?.navigationBar.barTintColor = .red
-            }
-            super.willMove(toParentViewController: parent)
-        }
-
-
-        private func setColors() {
-            navigationController?.navigationBar.tintColor = .black
-            navigationController?.navigationBar.barTintColor = .blue
-            navigationController?.navigationBar.isTranslucent = false
-        }
+    override func loadView() {
+        super.loadView()
+        title = "B"
+        view.backgroundColor = .white
     }
+
+    override func viewWillAppear(_ animated: Bool) {
+        setColors()
+        applyNavigationBarHeight(100)
+        super.viewWillAppear(animated)
+    }
+
+    override func willMove(toParentViewController parent: UIViewController?) {
+        if parent == nil {
+            navigationController?.navigationBar.barTintColor = .red
+            applyNavigationBarHeight(44)
+        }
+        super.willMove(toParentViewController: parent)
+    }
+
+    private func setColors() {
+        navigationController?.navigationBar.tintColor = .black
+        navigationController?.navigationBar.barTintColor = .blue
+        navigationController?.navigationBar.isTranslucent = false
+    }
+}
+
+extension UIViewController {
+    func applyNavigationBarHeight(_ height: CGFloat) {
+        guard let coordinator = self.transitionCoordinator else {
+            return
+        }
+        coordinator.animate(alongsideTransition: {
+            [weak self] context in
+
+            context.view(forKey: .from)
+            self?.navigationController?.navigationBar.apply(height: height)
+        }, completion: nil)
+    }
+}
+
+
+extension UINavigationBar {
+    func apply(height: CGFloat) {
+        var navFrame = frame
+        navFrame.size.height = height
+        frame = navFrame
+    }
+}
+
